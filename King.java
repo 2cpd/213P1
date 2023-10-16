b404b4ac414dfb612a9fa907907704a66c5d6b3d
2cpd <147893675+2cpd@users.noreply.github.com>
2023-10-15 23:24:43 -0400

    Add files via upload

--- King.java
+++ King.java
@@ -4,9 +4,10 @@ import java.lang.Math;
 import java.util.ArrayList;
 
 public class King extends Piece{
+	boolean isCastle = false;
 	
 	public King(ReturnPiece currPiece, String move, ArrayList<ReturnPiece> list) {
-		super(currPiece, move, list);
+		super(currPiece, move, list); 
 	}
 	
 	public boolean isValidMove() {
@@ -16,14 +17,26 @@ public class King extends Piece{
 		}
 		
 		if (!this.isBlocked()) {
-			int fileDiff = Math.abs(tarFile - currFile);
-			int rankDiff = Math.abs(tarRank - currRank);
+			int fileDiff = Math.abs(tarFile - currFile); int rankDiff = Math.abs(tarRank - currRank);
+			if (rankDiff == 0 && fileDiff == 2) {
+				isCastle = true;
+				return true;
+			}
 			
 			if (fileDiff == 1 || rankDiff == 1) {
-				if (fileDiff*rankDiff == 1 || fileDiff*rankDiff == 0) return true;
+				if (fileDiff*rankDiff == 1 || fileDiff*rankDiff == 0)
+					isCastle = false;
+					return true;
 			}
 		}
 		return false;		
+	}
+	
+	public boolean isInCheck() {
+		//Horizontally: R/Q
+		//Diagonally
+		ReturnPiece checkingPiece;
+		return true;
 	}
 }
 
