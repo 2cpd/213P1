dbb41896cc4bbb7de2d3926a2b1fde468ee4ce98
2cpd <147893675+2cpd@users.noreply.github.com>
2023-10-15 23:01:59 -0400

    Add files via upload

--- Piece.java
+++ Piece.java
@@ -2,8 +2,7 @@ package chess;
 import java.util.ArrayList;
 
 public class Piece extends Chess {
-	char currColor;
-	int isWhite; //1 if white, 0 if black
+	char currColor; int isWhite; //1 if white, 0 if black
 	String move; ReturnPiece currPiece; ReturnPiece tarPiece;
 	ArrayList<ReturnPiece> piecesList;
 	
@@ -11,20 +10,21 @@ public class Piece extends Chess {
 	//this will return integer value from 1 to 8 for file for a to h
 	int currRank = move.charAt(1) - '0'; int tarRank = move.charAt(4) - '0';
 	
-	public Piece(ReturnPiece currPiece, String move) {
-		this.currPiece = currPiece; this.move = move;
-		currColor = currPiece.pieceType.toString().charAt(0);
-		
-		if (currPiece.pieceType.toString().charAt(0) == 'W') isWhite = 1;
-		else isWhite = 0;
-	}
-	
 	public Piece(ReturnPiece currPiece, String move, ArrayList<ReturnPiece> list) {
 		this.currPiece = currPiece; this.move = move; this.piecesList = list;
 		currColor = currPiece.pieceType.toString().charAt(0);
 		
 		if (currPiece.pieceType.toString().charAt(0) == 'W') isWhite = 1;
 		else isWhite = 0;
+		
+		for (int i = 0; i < piecesList.size(); i++) {
+			ReturnPiece checkingPiece = piecesList.get(i);
+			if (tarFile == checkingPiece.toString().charAt(0)-'`' &&
+					tarRank == checkingPiece.toString().charAt(2)-'0') {
+				tarPiece = checkingPiece;
+				break;
+			}
+		}
 	}
 	
 	public boolean isValidMove() {
@@ -35,7 +35,7 @@ public class Piece extends Chess {
 	}
 	
 	public boolean isBlocked() { //default blocked check
-		if (tarPiece != null && currColor == currPiece.pieceType.toString().charAt(0)) {
+		if (tarPiece != null && currColor == currPiece.toString().charAt(3)) {
 			return true;
 		}
 		return false;
