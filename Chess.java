a0ae75fb62c3af67db37d8a1831c0d05c247e48e
2cpd <147893675+2cpd@users.noreply.github.com>
2023-10-16 19:20:31 -0400

    Add files via upload

--- Chess.java
+++ Chess.java
@@ -76,7 +76,7 @@ public class Chess {
 		if (currPlayer == Chess.Player.white) playerIsWhite = 1; //if curr player is white
 		else playerIsWhite = 0; //if black
 		
-		for (int i = 0; i < piecesList.size(); i++) { //move chess
+		for (int i = 0; i < piecesList.size(); i++) { //find kings
 			ReturnPiece currReturnPiece = piecesList.get(i);
 			if (currReturnPiece.pieceType == ReturnPiece.PieceType.BK) { //if BK
 				blackKing = new King (currReturnPiece, move, piecesList);
@@ -86,13 +86,13 @@ public class Chess {
 			}
 		}
 		
-		for (int i = 0; i < piecesList.size(); i++) { //move chess
+		for (int i = 0; i < piecesList.size(); i++) { //find piece
 			ReturnPiece currReturnPiece = piecesList.get(i);
 			
 			if (currReturnPiece.pieceFile.toString().charAt(0) == move.charAt(0) && //column
 				currReturnPiece.pieceRank == move.charAt(1) - '0') { // row //find the piece
 				
-				/*****************************create chess type******************************/
+					/**create chess type**/
 					if (currReturnPiece.pieceType == ReturnPiece.PieceType.BK ||
 						currReturnPiece.pieceType == ReturnPiece.PieceType.WK) { //if king
 							thisPiece = new King (currReturnPiece, move, piecesList);
@@ -122,15 +122,40 @@ public class Chess {
 						currReturnPiece.pieceType == ReturnPiece.PieceType.WR) { //if rook
 							thisPiece = new Rook (currReturnPiece, move, piecesList);
 					}
-					
-					boolean requestedMoveValid = thisPiece.isValidMove();
-					/*****************************create chess type******************************/
-				
-					if (thisPiece.isWhite == playerIsWhite) { //if the players move their own chess or the other's
+					else { //no Piece on that tile
 						temp.piecesOnBoard = piecesList;
 						System.out.print(ReturnPlay.Message.ILLEGAL_MOVE); 
 						return temp;
 					}
+					
+					boolean requestedMoveValid = thisPiece.isValidMove();
+					System.out.println("testmessage");
+					System.out.println(thisPiece == null);
+					/**create chess type**/
+					
+					//if currColor != playerColor
+					if (thisPiece.isWhite != playerIsWhite) {
+						temp.piecesOnBoard = piecesList;
+						System.out.print(ReturnPlay.Message.ILLEGAL_MOVE); 
+						return temp;
+					}
+					
+					////if currColor != playerColor
+					if (thisPiece.tarFile > 8 || thisPiece.tarFile < 1 || thisPiece.tarRank > 8 || thisPiece.tarRank < 1) {
+						temp.piecesOnBoard = piecesList;
+						System.out.print(ReturnPlay.Message.ILLEGAL_MOVE); 
+						return temp;
+					}
+					
+					/**move**/
+					tempPiece.pieceType = currReturnPiece.pieceType; 
+					
+					tempPiece.pieceFile = findFile(move.charAt(3));
+					
+					tempPiece.pieceRank = move.charAt(4) - '0';
+					piecesList.remove(i);
+					piecesList.add(tempPiece);
+					/**move**/
 				
 					if (currReturnPiece.pieceType.toString() == "WK"|| currReturnPiece.pieceType.toString() == "BK") { //check castling
 						King tempKing = new King(currReturnPiece, move, piecesList);
@@ -143,15 +168,7 @@ public class Chess {
 						}
 					} 
 				
-					/***********move*************/
-					tempPiece.pieceType = piecesList.get(i).pieceType; 
 					
-					tempPiece.pieceFile = findFile(move.charAt(3));
-					
-					tempPiece.pieceRank = move.charAt(4) - '0';
-					piecesList.remove(i);
-					piecesList.add(tempPiece);
-					/***************move**************/
 					
 					/*************find enemy chess and kill it****************/
 					for (int j = 0; j < piecesList.size(); j++) {
@@ -165,7 +182,7 @@ public class Chess {
 					}
 					/*************find enemy chess and kill it****************/
 					
-					if (move.length() == 11 && move.substring(6, 11) == "draws?") { //propose draw
+					if (move.length() == 11 && move.substring(6, 11) == "draw?") { //propose draw
 						System.out.print(ReturnPlay.Message.DRAW);
 						return null; //need revise here, return ?
 					}
@@ -179,10 +196,12 @@ public class Chess {
 		
 		if (currPlayer == Chess.Player.white) { //switch side
 			currPlayer = Chess.Player.black;
-		} else {
-			currPlayer = Chess.Player.white;
+			System.out.println("Change to black");
 		}
-		
+		else {
+			currPlayer = Chess.Player.white;
+			System.out.println("Change to white");
+		}
 		
 		/* FOLLOWING LINE IS A PLACEHOLDER TO MAKE COMPILER HAPPY */
 		/* WHEN YOU FILL IN THIS METHOD, YOU NEED TO RETURN A ReturnPlay OBJECT */
@@ -316,6 +335,8 @@ public class Chess {
 	public static void start() {
 		/* FILL IN THIS METHOD */
 		ArrayList<ReturnPiece> pieces = new ArrayList<ReturnPiece>();
+		currPlayer = Chess.Player.white;
+		System.out.println("Setting player to white");
 		
 		for (int i = 1; i <= 32; i++) {
 			ReturnPiece temp = new ReturnPiece();
